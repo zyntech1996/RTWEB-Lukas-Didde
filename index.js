fbe0cc621c5cdb762f8b16b7026384ec5f9636e6
zyntech1996 <32703492+zyntech1996@users.noreply.github.com>
2020-03-03 16:39:26 +0100

    Game file works now

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/game', (req, res) => res.render('pages/game'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+ 
