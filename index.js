2ce376cbec18ad99be9a8a1617ab8d06cea451c5
zyntech1996 <32703492+zyntech1996@users.noreply.github.com>
2020-03-06 07:02:33 +0100

    not working atm

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 //var db = require("./mongoDataBase");
-var MongoClient = require('mongodb').MongoClient;
+/*var MongoClient = require('mongodb').MongoClient;
 var uri = "mongodb+srv://lutei:Master12345@cluster0-clnev.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
 
@@ -13,7 +13,7 @@ MongoClient.connect(uri, function(err, db) {
       //console.log("1 document inserted");
       db.close();
   });
-
+*/
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -23,7 +23,7 @@ express()
   .get('/db', (req, res) => res.render('pages/db'))
   .get('/gamelogic.js', (req, res) => res.sendFile(path.join(__dirname, 'views/pages/gamelogic.js')))
   .get('/highscores.json', (req, res) => res.sendFile(path.join(__dirname, 'highscores.json')))
-  .get('/mongoDataBase', (req, res) => res.sendFile(path.join(__dirname, 'mongoDataBase.js')))
+  //.get('/mongoDataBase', (req, res) => res.sendFile(path.join(__dirname, 'mongoDataBase.js')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
