dcdcef74a6eb898d60cc3560054677e2ff7f62f8
zyntech1996 <32703492+zyntech1996@users.noreply.github.com>
2020-03-10 21:08:01 +0100

    updating index.js

--- index.js
+++ index.js
@@ -1,4 +1,68 @@
-const express = require('express')
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var router = require('./router');
+const path = require('path');
+var userRoutes = require('./routes');
+
+
+//connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/db');
+var db = mongoose.connection;
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!'
+    console.log("DB connected!");
+});
+
+//use sessions for tracking logins
+app.use(session({
+    secret: 'work hard',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection: db
+    })
+}));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// serve static files from template
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use('/', router);
+app.use('/', userRoutes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message);
+});
+
+// listen on port 3000
+app.listen(3000, function () {
+    console.log('Express app listening on port 3000');
+});
+/*const express = require('express')
 const path = require('path')
 var app = express();
 var router = require('./routes');
@@ -11,7 +75,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use('/', router);
 
-var server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+var server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));*/
 
 
 
